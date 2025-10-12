0f15af618d6570a02aeb49ccdfc8822df201624b
tirumalateja19 <tirumalateja.jampani@gmail.com>
2025-10-12 09:27:06 +0530

    exploring HTTP methods

--- src/server.js
+++ src/server.js
@@ -3,10 +3,19 @@ const app = express();
 const port = 5656;
 
 app.get("/", (req, res) => {
-  res.send(" Nodejs 🚀🚀!!");
+  res.send("Server Running");
 });
 app.get("/test", (req, res) => {
-  res.send("Nodejs Backend of DevZone");
+  //fetching data
+  res.send("Data fetched successfully");
+});
+app.post("/test", (req, res) => {
+  //logic for saving data
+  res.send("Data saved successfully");
+});
+app.delete("/test", (req, res) => {
+  //logic for deleting data
+  res.send("Data Deleted successfully");
 });
 app.listen(port, () => {
   console.log("Server successfully running at port:", port);
